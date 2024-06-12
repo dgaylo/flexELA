50bb284e64117732ec421159ad154f1298c57a15
Declan Gaylo <dgaylo@mit.edu>
2024-06-12 16:47:06 -0400

    Formated src/svector

--- src/svector/svector.cpp
+++ src/svector/svector.cpp
@@ -1,30 +1,31 @@
 #include "svector.h"
 
 #include <algorithm>
-#include <utility>
 #include <limits>
+#include <utility>
 
 using namespace svec;
 
-SVector::SVector(const Element* const buff) {
+SVector::SVector(const Element* const buff)
+{
     // Find the end of the buffer
     const Element* itr = buff;
 
-    while( ! itr->isEnd() ) {
+    while (!itr->isEnd()) {
         itr++;
 
-        //check that labels are actually in order
+        // check that labels are actually in order
         assert(itr[0].l > itr[-1].l);
     }
 
     // copy into the vector
-    vec.assign(buff,itr);
+    vec.assign(buff, itr);
 }
 
 Value SVector::sum() const
 {
     Value sum = 0.0;
-    for(auto elm : vec) {
+    for (auto elm : vec) {
         sum += elm.v;
     }
     return sum;
@@ -33,9 +34,8 @@ Value SVector::sum() const
 Value SVector::getMinValue() const
 {
     Value minimum = std::numeric_limits<Value>::max();
-    for(auto elm : vec) {
-        if(elm.v < minimum)
-            minimum=elm.v;
+    for (auto elm : vec) {
+        if (elm.v < minimum) minimum = elm.v;
     }
     return minimum;
 }
@@ -43,48 +43,46 @@ Value SVector::getMinValue() const
 Value SVector::getMaxValue() const
 {
     Value maximum = std::numeric_limits<Value>::min();
-    for(auto elm : vec) {
-        if(elm.v > maximum)
-            maximum=elm.v;
+    for (auto elm : vec) {
+        if (elm.v > maximum) maximum = elm.v;
     }
     return maximum;
 }
 
 Label svec::SVector::getMaxLabel() const
 {
-    if(vec.empty()) return 0;
+    if (vec.empty()) return 0;
 
     return vec.back().l;
 }
 
-void SVector::add(const SVector &a, const Value &C)
+void SVector::add(const SVector& a, const Value& C)
 {
     // quick exit
-    if(a.isEmpty() || C==0.0) return;
+    if (a.isEmpty() || C == 0.0) return;
 
     // Could be more memory efficient, but using a temporay variable for now
     SVector tmp = fma(a, C, *this);
     this->vec = std::move(tmp.vec);
 }
 
-void SVector::normalize(const Value &total)
+void SVector::normalize(const Value& total)
 {
     // quick exit
-    if(isEmpty()) return;
+    if (isEmpty()) return;
 
-    
     const Value s = sum();
 
-    // if normalizing to zero, empty vector 
+    // if normalizing to zero, empty vector
     // empty vector rather than divide by zero
-    if(total==0 || s==0.0) {
+    if (total == 0 || s == 0.0) {
         clear();
         return;
     }
 
     // normalize each value so sum(s)=total;
-    const Value factor = total/s;
-    for(auto &elm : vec) {
+    const Value factor = total / s;
+    for (auto& elm : vec) {
         elm *= factor;
     }
 }
@@ -92,22 +90,22 @@ void SVector::normalize(const Value &total)
 void SVector::chop()
 {
     // set any value less than zero to zero
-    for(auto &elm : vec) {
-        if(elm.v<0.0) {
-            elm.v=0.0;
+    for (auto& elm : vec) {
+        if (elm.v < 0.0) {
+            elm.v = 0.0;
         }
     }
 }
 
-void svec::SVector::zeroEntry(const Label &l)
+void svec::SVector::zeroEntry(const Label& l)
 {
-    auto itr=vec.begin();
-    while(itr!= vec.cend()) {
-        if(itr->l == l) {
+    auto itr = vec.begin();
+    while (itr != vec.cend()) {
+        if (itr->l == l) {
             vec.erase(itr);
             return;
         }
-        else if(itr->l > l) {
+        else if (itr->l > l) {
             return;
         }
 
@@ -115,69 +113,69 @@ void svec::SVector::zeroEntry(const Label &l)
     }
 }
 
-SVector svec::fma(const SVector &lhs, const Value& C, const SVector &rhs)
+SVector svec::fma(const SVector& lhs, const Value& C, const SVector& rhs)
 {
-    SVector out= SVector();
+    SVector out = SVector();
 
     // references to the underlying vectors
-    const std::vector<Element> &vecL = lhs.vec;
-    const std::vector<Element> &vecR = rhs.vec;
+    const std::vector<Element>& vecL = lhs.vec;
+    const std::vector<Element>& vecR = rhs.vec;
 
     // use max size of rhs and lhs to reserve size of out vector
-    out.vec.reserve( std::max(vecL.size(), vecR.size()) );
+    out.vec.reserve(std::max(vecL.size(), vecR.size()));
 
     auto itrL = vecL.cbegin();
     auto itrR = vecR.cbegin();
-    
+
     // merge sort
-    while(itrL!=vecL.cend() && itrR!=vecR.cend()) {
+    while (itrL != vecL.cend() && itrR != vecR.cend()) {
         const svec::Label& labelL = itrL->l;
-        const svec::Label&  labelR = itrR->l;
+        const svec::Label& labelR = itrR->l;
 
         if (labelL < labelR)
-            out.vec.emplace_back( (*itrL++)*C );
+            out.vec.emplace_back((*itrL++) * C);
         else if (labelL > labelR)
-            out.vec.emplace_back( (*itrR++) );
+            out.vec.emplace_back((*itrR++));
         else
-            out.vec.emplace_back( fma(*itrL++,C,*itrR++) );
+            out.vec.emplace_back(fma(*itrL++, C, *itrR++));
     }
-    while(itrL!=vecL.cend()) {
-        out.vec.emplace_back( (*itrL++)*C );
+    while (itrL != vecL.cend()) {
+        out.vec.emplace_back((*itrL++) * C);
     }
-    while(itrR!=vecR.cend()) {
-        out.vec.emplace_back( (*itrR++) );
+    while (itrR != vecR.cend()) {
+        out.vec.emplace_back((*itrR++));
     }
-    
+
     return out;
 }
 
-SVector svec::operator/(const SVector &a, const Value &C)
+SVector svec::operator/(const SVector& a, const Value& C)
 {
     SVector out = a;
-    for(auto &elm : out.vec) {
+    for (auto& elm : out.vec) {
         elm /= C;
     }
     return out;
 }
 
-SVector svec::operator*(const SVector &a, const Value &C)
+SVector svec::operator*(const SVector& a, const Value& C)
 {
     SVector out = a;
-    for(auto &elm : out.vec) {
+    for (auto& elm : out.vec) {
         elm *= C;
     }
     return out;
 }
 
-SVector svec::normalize(const SVector &a, const Value &total)
+SVector svec::normalize(const SVector& a, const Value& total)
 {
     const Value s = a.sum();
-    
-    // if normalizing to zero, empty vector 
+
+    // if normalizing to zero, empty vector
     // empty vector rather than divide by zero
-    if(total==0.0 || s==0.0) {
+    if (total == 0.0 || s == 0.0) {
         return SVector();
     }
 
-    return a*(total/s);
+    return a * (total / s);
 }
--- src/svector/tests/svector_test.cpp
+++ src/svector/tests/svector_test.cpp
@@ -1,10 +1,10 @@
-#include <gtest/gtest.h>
 #include "../svector.h"
+#include <gtest/gtest.h>
 
 #include <cstdlib>
 
-constexpr svec::Label TEST_LABEL_A=23;
-constexpr svec::Value TEST_VALUE_A=0.1;
+constexpr svec::Label TEST_LABEL_A = 23;
+constexpr svec::Value TEST_VALUE_A = 0.1;
 
 svec::Value fRand(svec::Value fMin, svec::Value fMax)
 {
@@ -12,86 +12,92 @@ svec::Value fRand(svec::Value fMin, svec::Value fMax)
     return fMin + f * (fMax - fMin);
 }
 
-TEST(SVectorTests, EmptyConstructor) {
+TEST(SVectorTests, EmptyConstructor)
+{
     svec::SVector s = svec::SVector();
 
-    EXPECT_EQ(s.NNZ(),0);
+    EXPECT_EQ(s.NNZ(), 0);
 }
 
-TEST(SVectorTests, SingleElementConstructor) {
-    svec::Element e = {TEST_LABEL_A,TEST_VALUE_A};
+TEST(SVectorTests, SingleElementConstructor)
+{
+    svec::Element e = {TEST_LABEL_A, TEST_VALUE_A};
     svec::SVector s = svec::SVector(e);
 
-    EXPECT_EQ(s.NNZ(),1);
+    EXPECT_EQ(s.NNZ(), 1);
 
-    const svec::Element* data=s.data();
-    EXPECT_EQ(data[0].l,TEST_LABEL_A);
-    EXPECT_EQ(data[0].v,TEST_VALUE_A);
+    const svec::Element* data = s.data();
+    EXPECT_EQ(data[0].l, TEST_LABEL_A);
+    EXPECT_EQ(data[0].v, TEST_VALUE_A);
 }
 
-TEST(SVectorTests, CopyConstructor) {
-    svec::Element e = {TEST_LABEL_A,TEST_VALUE_A};
+TEST(SVectorTests, CopyConstructor)
+{
+    svec::Element e = {TEST_LABEL_A, TEST_VALUE_A};
     const svec::SVector s = svec::SVector(e);
 
     svec::SVector s2 = svec::SVector(s);
 
-    EXPECT_EQ(s2.NNZ(),1);
+    EXPECT_EQ(s2.NNZ(), 1);
 
-    const svec::Element* data=s2.data();
-    EXPECT_EQ(data[0].l,TEST_LABEL_A);
-    EXPECT_EQ(data[0].v,TEST_VALUE_A);
+    const svec::Element* data = s2.data();
+    EXPECT_EQ(data[0].l, TEST_LABEL_A);
+    EXPECT_EQ(data[0].v, TEST_VALUE_A);
 }
 
-TEST(SVectorTests, MoveConstructor) {
-    svec::Element e = {TEST_LABEL_A,TEST_VALUE_A};
+TEST(SVectorTests, MoveConstructor)
+{
+    svec::Element e = {TEST_LABEL_A, TEST_VALUE_A};
     const svec::SVector s = svec::SVector(e);
 
     svec::SVector s2 = svec::SVector(std::move(s));
 
-    EXPECT_EQ(s2.NNZ(),1);
+    EXPECT_EQ(s2.NNZ(), 1);
 
-    const svec::Element* data=s2.data();
-    EXPECT_EQ(data[0].l,TEST_LABEL_A);
-    EXPECT_EQ(data[0].v,TEST_VALUE_A);
+    const svec::Element* data = s2.data();
+    EXPECT_EQ(data[0].l, TEST_LABEL_A);
+    EXPECT_EQ(data[0].v, TEST_VALUE_A);
 }
 
-TEST(SVectorTests, MultiElementConstructor) {
+TEST(SVectorTests, MultiElementConstructor)
+{
     constexpr size_t length = 5;
 
     // create a buffer with two svectors in it
-    svec::Element* buff = new svec::Element[length+1];
-    for(std::size_t i=0; i<length; i++) {
-        buff[i].l=i;
-        buff[i].v=fRand(0.0,1.0);
+    svec::Element* buff = new svec::Element[length + 1];
+    for (std::size_t i = 0; i < length; i++) {
+        buff[i].l = i;
+        buff[i].v = fRand(0.0, 1.0);
     }
-    buff[length]=svec::END_ELEMENT;
+    buff[length] = svec::END_ELEMENT;
 
     svec::SVector s = svec::SVector(buff);
 
-    EXPECT_EQ(s.NNZ(),length);
+    EXPECT_EQ(s.NNZ(), length);
 
-    for(std::size_t i=0; i<length; i++) {
-        EXPECT_EQ(buff[i].l,s.data()[i].l);
-        EXPECT_EQ(buff[i].v,s.data()[i].v);
+    for (std::size_t i = 0; i < length; i++) {
+        EXPECT_EQ(buff[i].l, s.data()[i].l);
+        EXPECT_EQ(buff[i].v, s.data()[i].v);
     }
 
     delete[] buff;
 }
 
-TEST(SVectorTests, Iterator) {
+TEST(SVectorTests, Iterator)
+{
     constexpr size_t length = 100;
 
-    svec::Element* buff = new svec::Element[length+1];
+    svec::Element* buff = new svec::Element[length + 1];
     svec::Value sum = 0.0;
     svec::Value sum2 = 0.0;
 
-    for(std::size_t i=0; i<length; i++) {
-        buff[i].l=i;
-        buff[i].v=fRand(0.0,1.0);
-        sum+=buff[i].v;
-        sum2+=buff[i].v*buff[i].v;
+    for (std::size_t i = 0; i < length; i++) {
+        buff[i].l = i;
+        buff[i].v = fRand(0.0, 1.0);
+        sum += buff[i].v;
+        sum2 += buff[i].v * buff[i].v;
     }
-    buff[length]=svec::END_ELEMENT;
+    buff[length] = svec::END_ELEMENT;
 
     svec::SVector s = svec::SVector(buff);
     delete[] buff;
@@ -99,219 +105,175 @@ TEST(SVectorTests, Iterator) {
     svec::Value sum_test = 0.0;
     svec::Value sum2_test = 0.0;
 
-    for(auto& elm : s) {
-        sum_test+=elm.v;
-        sum2_test+=elm.v*elm.v;
+    for (auto& elm : s) {
+        sum_test += elm.v;
+        sum2_test += elm.v * elm.v;
     }
 
-    EXPECT_DOUBLE_EQ(sum,sum_test);
-    EXPECT_DOUBLE_EQ(sum2,sum2_test);
+    EXPECT_DOUBLE_EQ(sum, sum_test);
+    EXPECT_DOUBLE_EQ(sum2, sum2_test);
 }
 
-TEST(SVectorTests, Sum) {
+TEST(SVectorTests, Sum)
+{
     constexpr size_t length = 5;
 
-    svec::Element* buff = new svec::Element[length+1];
+    svec::Element* buff = new svec::Element[length + 1];
     svec::Value sum = 0.0;
 
-    for(std::size_t i=0; i<length; i++) {
-        buff[i].l=i;
-        buff[i].v=fRand(0.0,1.0);
-        sum+=buff[i].v;
+    for (std::size_t i = 0; i < length; i++) {
+        buff[i].l = i;
+        buff[i].v = fRand(0.0, 1.0);
+        sum += buff[i].v;
     }
-    buff[length]=svec::END_ELEMENT;
+    buff[length] = svec::END_ELEMENT;
 
-    EXPECT_DOUBLE_EQ(svec::SVector(buff).sum(),sum);
+    EXPECT_DOUBLE_EQ(svec::SVector(buff).sum(), sum);
     delete[] buff;
 
     // confirm nothing weird happens with empty svectors
-    EXPECT_EQ(svec::SVector().sum(),0.0);
-    EXPECT_EQ(svec::SVector(svec::Element({1,0.0})).sum(),0.0);
+    EXPECT_EQ(svec::SVector().sum(), 0.0);
+    EXPECT_EQ(svec::SVector(svec::Element({1, 0.0})).sum(), 0.0);
 }
 
-TEST(SVectorTests, Add) {
-    svec::Element buff1[4] = {
-        {1,0.1},
-        {3,0.2},
-        {4,0.8},
-        svec::END_ELEMENT
-    };
-    svec::SVector s1=svec::SVector(buff1);
+TEST(SVectorTests, Add)
+{
+    svec::Element buff1[4] = {{1, 0.1}, {3, 0.2}, {4, 0.8}, svec::END_ELEMENT};
+    svec::SVector s1 = svec::SVector(buff1);
 
-    svec::Element buff2[4] = {
-        {2,0.8},
-        {3,0.3},
-        {8,0.4},
-        svec::END_ELEMENT
-    };
-    svec::SVector s2=svec::SVector(buff2);
+    svec::Element buff2[4] = {{2, 0.8}, {3, 0.3}, {8, 0.4}, svec::END_ELEMENT};
+    svec::SVector s2 = svec::SVector(buff2);
 
     svec::Element solution[5] = {
-        {1,0.1*0.5},
-        {2,0.0*0.5+0.8},
-        {3,0.2*0.5+0.3},
-        {4,0.8*0.5+0.0},
-        {8,0.4}
-    };
+        {1, 0.1 * 0.5}, {2, 0.0 * 0.5 + 0.8}, {3, 0.2 * 0.5 + 0.3}, {4, 0.8 * 0.5 + 0.0}, {8, 0.4}};
 
-    s2.add(s1,0.5);
+    s2.add(s1, 0.5);
 
-    for(auto i=0; i<5; i++) {
-        EXPECT_EQ(s2.data()[i].l,solution[i].l);
-        EXPECT_DOUBLE_EQ(s2.data()[i].v,solution[i].v);
+    for (auto i = 0; i < 5; i++) {
+        EXPECT_EQ(s2.data()[i].l, solution[i].l);
+        EXPECT_DOUBLE_EQ(s2.data()[i].v, solution[i].v);
     }
 
     // Try again, but reverse which being multiplied
     svec::Element solution2[5] = {
-        {1,0.1},
-        {2,0.8*0.6},
-        {3,0.3*0.6+0.2},
-        {4,0.8},
-        {8,0.4*0.6}
-    };
-    s2=svec::SVector(buff2);
+        {1, 0.1}, {2, 0.8 * 0.6}, {3, 0.3 * 0.6 + 0.2}, {4, 0.8}, {8, 0.4 * 0.6}};
+    s2 = svec::SVector(buff2);
 
-    s1.add(s2,0.6);
+    s1.add(s2, 0.6);
 
-    for(auto i=0; i<5; i++) {
-        EXPECT_EQ(s1.data()[i].l,solution2[i].l);
-        EXPECT_DOUBLE_EQ(s1.data()[i].v,solution2[i].v);
+    for (auto i = 0; i < 5; i++) {
+        EXPECT_EQ(s1.data()[i].l, solution2[i].l);
+        EXPECT_DOUBLE_EQ(s1.data()[i].v, solution2[i].v);
     }
 }
 
-TEST(SVectorTests, Normalize) {
+TEST(SVectorTests, Normalize)
+{
     constexpr size_t length = 5;
 
-    svec::Element* buff = new svec::Element[length+1];
+    svec::Element* buff = new svec::Element[length + 1];
     svec::Value sum = 0.0;
 
-    for(std::size_t i=0; i<length; i++) {
-        buff[i].l=i;
-        buff[i].v=fRand(0.0,1.0);
-        sum+=buff[i].v;
+    for (std::size_t i = 0; i < length; i++) {
+        buff[i].l = i;
+        buff[i].v = fRand(0.0, 1.0);
+        sum += buff[i].v;
     }
-    buff[length]=svec::END_ELEMENT;
+    buff[length] = svec::END_ELEMENT;
 
     svec::SVector s = svec::SVector(buff);
     s.normalize(0.3);
 
-    for(std::size_t i=0; i<length; i++) {
-        EXPECT_EQ(buff[i].l,s.data()[i].l);
-        EXPECT_DOUBLE_EQ(buff[i].v*(0.3/sum),s.data()[i].v);
+    for (std::size_t i = 0; i < length; i++) {
+        EXPECT_EQ(buff[i].l, s.data()[i].l);
+        EXPECT_DOUBLE_EQ(buff[i].v * (0.3 / sum), s.data()[i].v);
     }
 
     svec::SVector s2 = svec::normalize(svec::SVector(buff), 0.3);
     ASSERT_EQ(s.NNZ(), s2.NNZ());
-    for(std::size_t i=0; i<length; i++) {
-        EXPECT_EQ(s.data()[i].l,s2.data()[i].l);
-        EXPECT_EQ(s.data()[i].v,s2.data()[i].v);
+    for (std::size_t i = 0; i < length; i++) {
+        EXPECT_EQ(s.data()[i].l, s2.data()[i].l);
+        EXPECT_EQ(s.data()[i].v, s2.data()[i].v);
     }
 
     // normalizing by zero should give an empty vector
     s = svec::SVector(buff);
     s.normalize(0.0);
-    EXPECT_EQ(s.NNZ(),0);
+    EXPECT_EQ(s.NNZ(), 0);
 
     s2 = svec::normalize(svec::SVector(buff), 0);
-    EXPECT_EQ(s2.NNZ(),0);
+    EXPECT_EQ(s2.NNZ(), 0);
 
     delete[] buff;
 
     // confirm nothing weird happens with empty vectors
     s = svec::SVector();
     s.normalize();
-    EXPECT_EQ(s.NNZ(),0);
-    EXPECT_EQ(s.sum(),0.0);
+    EXPECT_EQ(s.NNZ(), 0);
+    EXPECT_EQ(s.sum(), 0.0);
 
-    s = svec::SVector(svec::Element({1,0.0}));
+    s = svec::SVector(svec::Element({1, 0.0}));
     s.normalize();
-    EXPECT_EQ(s.NNZ(),0);
-    EXPECT_EQ(s.sum(),0.0);
+    EXPECT_EQ(s.NNZ(), 0);
+    EXPECT_EQ(s.sum(), 0.0);
 
-    s2 = normalize(svec::Element({1,0.0}));
-    EXPECT_EQ(s2.NNZ(),0);
-    EXPECT_EQ(s2.sum(),0.0);
+    s2 = normalize(svec::Element({1, 0.0}));
+    EXPECT_EQ(s2.NNZ(), 0);
+    EXPECT_EQ(s2.sum(), 0.0);
 
     s2 = normalize(svec::SVector());
-    EXPECT_EQ(s2.NNZ(),0);
-    EXPECT_EQ(s2.sum(),0.0);
+    EXPECT_EQ(s2.NNZ(), 0);
+    EXPECT_EQ(s2.sum(), 0.0);
 }
 
-TEST(SVectorTests, FMA) {
-    svec::Element buff1[4] = {
-        {1,0.1},
-        {3,0.2},
-        {4,0.8},
-        svec::END_ELEMENT
-    };
-    svec::SVector s1=svec::SVector(buff1);
+TEST(SVectorTests, FMA)
+{
+    svec::Element buff1[4] = {{1, 0.1}, {3, 0.2}, {4, 0.8}, svec::END_ELEMENT};
+    svec::SVector s1 = svec::SVector(buff1);
 
-    svec::Element buff2[4] = {
-        {2,0.8},
-        {3,0.3},
-        {8,0.4},
-        svec::END_ELEMENT
-    };
-    svec::SVector s2=svec::SVector(buff2);
+    svec::Element buff2[4] = {{2, 0.8}, {3, 0.3}, {8, 0.4}, svec::END_ELEMENT};
+    svec::SVector s2 = svec::SVector(buff2);
 
     svec::Element solution[5] = {
-        {1,0.1*0.5},
-        {2,0.0*0.5+0.8},
-        {3,0.2*0.5+0.3},
-        {4,0.8*0.5+0.0},
-        {8,0.4}
-    };
+        {1, 0.1 * 0.5}, {2, 0.0 * 0.5 + 0.8}, {3, 0.2 * 0.5 + 0.3}, {4, 0.8 * 0.5 + 0.0}, {8, 0.4}};
 
+    svec::SVector s3 = svec::fma(s1, 0.5, s2);
 
-    svec::SVector s3 = svec::fma(s1,0.5,s2);
-
-    for(auto i=0; i<5; i++) {
-        EXPECT_EQ(s3.data()[i].l,solution[i].l);
-        EXPECT_DOUBLE_EQ(s3.data()[i].v,solution[i].v);
+    for (auto i = 0; i < 5; i++) {
+        EXPECT_EQ(s3.data()[i].l, solution[i].l);
+        EXPECT_DOUBLE_EQ(s3.data()[i].v, solution[i].v);
     }
 
     // Try again, but reverse which being multiplied
     svec::Element solution2[5] = {
-        {1,0.1},
-        {2,0.8*0.6},
-        {3,0.3*0.6+0.2},
-        {4,0.8},
-        {8,0.4*0.6}
-    };
+        {1, 0.1}, {2, 0.8 * 0.6}, {3, 0.3 * 0.6 + 0.2}, {4, 0.8}, {8, 0.4 * 0.6}};
 
-    s3 = svec::fma(s2,0.6,s1);
+    s3 = svec::fma(s2, 0.6, s1);
 
-    for(auto i=0; i<5; i++) {
-        EXPECT_EQ(s3.data()[i].l,solution2[i].l);
-        EXPECT_DOUBLE_EQ(s3.data()[i].v,solution2[i].v);
+    for (auto i = 0; i < 5; i++) {
+        EXPECT_EQ(s3.data()[i].l, solution2[i].l);
+        EXPECT_DOUBLE_EQ(s3.data()[i].v, solution2[i].v);
     }
 }
 
-TEST(SVectorTests, zeroEntry) {
-    svec::Element buff[6] = {
-        {0,0.1},
-        {1,0.1},
-        {3,0.2},
-        {4,0.8},
-        {6,0.3},
-        svec::END_ELEMENT
-    };
-    svec::SVector s=svec::SVector(buff);
+TEST(SVectorTests, zeroEntry)
+{
+    svec::Element buff[6] = {{0, 0.1}, {1, 0.1}, {3, 0.2}, {4, 0.8}, {6, 0.3}, svec::END_ELEMENT};
+    svec::SVector s = svec::SVector(buff);
 
     // should not change anything
     s.zeroEntry(5);
-    ASSERT_EQ(s.NNZ(),5);
+    ASSERT_EQ(s.NNZ(), 5);
 
     // should not change anything
     s.zeroEntry(8);
-    ASSERT_EQ(s.NNZ(),5);
+    ASSERT_EQ(s.NNZ(), 5);
 
     // remove zero at the start
     s.zeroEntry(0);
-    ASSERT_EQ(s.NNZ(),4);
-    for(auto i=0; i<4; i++) {
-        EXPECT_EQ(s.data()[i].l,buff[i+1].l);
-        EXPECT_EQ(s.data()[i].v,buff[i+1].v);
+    ASSERT_EQ(s.NNZ(), 4);
+    for (auto i = 0; i < 4; i++) {
+        EXPECT_EQ(s.data()[i].l, buff[i + 1].l);
+        EXPECT_EQ(s.data()[i].v, buff[i + 1].v);
     }
-
 }
