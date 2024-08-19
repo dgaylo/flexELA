90ac15f157d31c8f03f6d96f1fc1bf5f40757f52
Declan Gaylo <dgaylo@mit.edu>
2024-08-19 11:11:45 -0400

    Added tests of getGhost() and getEdge()

--- src/domain/tests/domain_test.cpp
+++ src/domain/tests/domain_test.cpp
@@ -52,6 +52,90 @@ TEST(DomainTests, Basic)
     delete (d);
 }
 
+TEST(DomainTests, getGhost)
+{
+    domain::Domain* d = new domain::Domain(NI, NJ, NK, NN);
+
+    // create dummy label feild
+    for (auto i = -1; i < NI + 1; ++i) {
+        for (auto j = -1; j < NJ + 1; ++j) {
+            for (auto k = -1; k < NK + 1; ++k) {
+                for (auto n = 0; n < NN; ++n) {
+                    d->s[n].at(i, j, k) = svec::SVector(
+                        svec::Element({static_cast<unsigned int>((i + 1) * (j + 3) * (k + 4)), 1.0})
+                    );
+                }
+            }
+        }
+    }
+
+    for (auto n = 0; n < NN; ++n) {
+        for (const auto& s : d->getGhost(domain::Face::iMinus, n)) {
+            ASSERT_EQ(s.data()->l, 0);
+        }
+        for (const auto& s : d->getGhost(domain::Face::jMinus, n)) {
+            ASSERT_EQ(s.data()->l % 2, 0);
+        }
+        for (const auto& s : d->getGhost(domain::Face::kMinus, n)) {
+            ASSERT_EQ(s.data()->l % 3, 0);
+        }
+
+        for (const auto& s : d->getGhost(domain::Face::iPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NI + 1), 0);
+        }
+        for (const auto& s : d->getGhost(domain::Face::jPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NJ + 3), 0);
+        }
+        for (const auto& s : d->getGhost(domain::Face::kPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NK + 4), 0);
+        }
+    }
+
+    delete (d);
+}
+
+TEST(DomainTests, getEdge)
+{
+    domain::Domain* d = new domain::Domain(NI, NJ, NK, NN);
+
+    // create dummy label feild
+    for (auto i = 0; i < NI; ++i) {
+        for (auto j = 0; j < NJ; ++j) {
+            for (auto k = 0; k < NK; ++k) {
+                for (auto n = 0; n < NN; ++n) {
+                    d->s[n].at(i, j, k) = svec::SVector(
+                        svec::Element({static_cast<unsigned int>((i) * (j + 2) * (k + 3)), 1.0})
+                    );
+                }
+            }
+        }
+    }
+
+    for (auto n = 0; n < NN; ++n) {
+        for (const auto& s : d->getEdge(domain::Face::iMinus, n)) {
+            ASSERT_EQ(s.data()->l, 0);
+        }
+        for (const auto& s : d->getEdge(domain::Face::jMinus, n)) {
+            ASSERT_EQ(s.data()->l % 2, 0);
+        }
+        for (const auto& s : d->getEdge(domain::Face::kMinus, n)) {
+            ASSERT_EQ(s.data()->l % 3, 0);
+        }
+
+        for (const auto& s : d->getEdge(domain::Face::iPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NI - 1), 0);
+        }
+        for (const auto& s : d->getEdge(domain::Face::jPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NJ + 1), 0);
+        }
+        for (const auto& s : d->getEdge(domain::Face::kPlus, n)) {
+            ASSERT_EQ(s.data()->l % (NK + 2), 0);
+        }
+    }
+
+    delete (d);
+}
+
 TEST(DomainTests, getOppositeFace)
 {
     using dir = domain::Face;
